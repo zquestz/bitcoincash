eda7aae9679ab6ec9e53307fda5dcc7ff8966a39
Corbin Fraser <corbin@commandbase.ca>
2017-09-17 17:16:29 +0900

    merged in alternative frontend built w/ foundation / scss / colour vars
    see /alternative/index.html

---
+++ alternative/js/app.js
@@ -0,0 +1,10 @@
+$(document).foundation();
+
+
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
