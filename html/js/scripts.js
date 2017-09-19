ec27e449066fa50b971fbb60593d6469ab5a82a5
Corbin Fraser <corbin@commandbase.ca>
2017-09-19 20:14:54 +0900

    Big batch of style updates.
    Revised images to use consistent sizing and square dimensions. Prevents certain orgs from having perceived preference. Added hover effects to href images, gave images some style. Added subtle section backgrounds to provide clear distinction between sections. Added JS scrollto ID's. Revised Fork Notice to alert in blue, keeps it a positive tone VS alerts or warnings which may have negative connotations. Revised typography in nav bar. Setup col-xs-6 on images so images are split into 2 columns on mobile. Scrolls were getting lengthy AF. Also ran some compression on the images to keep things snappy.
    
    Note that this pull-request is dependant on /alternative/ layout Pull Request as revised logo images are all hanging out there. If you'd like to keep only this one please let me know and I'll merge the images into main /html/

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,3 +1,13 @@
+// scroll to sections
+
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
+
 
 window.mr = window.mr || {};
 
